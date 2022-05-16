82e552030b567d5ba70dc14139d065c52cac9998
BeniceBB <info@benicebot.nl>
2022-05-16 16:47:32 +0200

    Setup crud

---
+++ src/Entity/Attendees.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AttendeesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AttendeesRepository::class)]
+class Attendees
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: Student::class, inversedBy: 'attendees')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $student;
+
+    #[ORM\ManyToOne(targetEntity: Trip::class, inversedBy: 'attendees')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $trip;
+
+    #[ORM\Column(type: 'integer')]
+    private $identity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStudent(): ?Student
+    {
+        return $this->student;
+    }
+
+    public function setStudent(?Student $student): self
+    {
+        $this->student = $student;
+
+        return $this;
+    }
+
+    public function getTrip(): ?Trip
+    {
+        return $this->trip;
+    }
+
+    public function setTrip(?Trip $trip): self
+    {
+        $this->trip = $trip;
+
+        return $this;
+    }
+
+    public function getIdentity(): ?int
+    {
+        return $this->identity;
+    }
+
+    public function setIdentity(int $identity): self
+    {
+        $this->identity = $identity;
+
+        return $this;
+    }
+}
--- src/Entity/Trip.php
+++ src/Entity/Trip.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TripRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TripRepository::class)]
@@ -30,6 +32,14 @@ class Trip
 
     #[ORM\Column(type: 'integer', nullable: true)]
     private $maxStudents;
+
+    #[ORM\OneToMany(mappedBy: 'tripId', targetEntity: Attendees::class)]
+    private $attendees;
+
+    public function __construct()
+    {
+        $this->attendees = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -104,6 +114,36 @@ class Trip
     public function setMaxStudents(?int $maxStudents): self
     {
         $this->maxStudents = $maxStudents;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Attendees>
+     */
+    public function getAttendees(): Collection
+    {
+        return $this->attendees;
+    }
+
+    public function addAttendee(Attendees $attendee): self
+    {
+        if (!$this->attendees->contains($attendee)) {
+            $this->attendees[] = $attendee;
+            $attendee->setTripId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAttendee(Attendees $attendee): self
+    {
+        if ($this->attendees->removeElement($attendee)) {
+            // set the owning side to null (unless already changed)
+            if ($attendee->getTripId() === $this) {
+                $attendee->setTripId(null);
+            }
+        }
 
         return $this;
     }
